5895d45b1c03f726f23b5822513eccfb722de898
Mark Henderson <mark@mrh.io>
2020-08-30 17:06:20 -0400

    refactor: move core functionality to index.js

--- cli.js
+++ cli.js
@@ -4,8 +4,9 @@ const gc = require('expose-gc/function')
 gc()
 
 /* global process */
-const os = require('os')
-const fs = require('fs')
+const benchmarks = require(process.cwd() + '/benchmarks')
+const { start } = require('./index')
+
 const yargs = require('yargs')
 const argv = yargs
   .usage('IPFS Log benchmark runner\n\nUsage: node --expose-gc $0 [options]')
@@ -59,127 +60,24 @@ const argv = yargs
   .example('$0 -r -g values-.*-baseline', 'Run all of the values baseline benchmarks')
   .argv
 
-const BASELINE_GREP = /[\d\w-]*-baseline/
-const DEFAULT_GREP = /.*/
-const grep = argv.grep ? new RegExp(argv.grep) : DEFAULT_GREP
-const stressLimit = argv.stressLimit || 300
-const baselineLimit = argv.baselineLimit || 1000
-const logLimit = argv.logLimit || 10000
+// const rimraf = (path) => {
+//   if (fs.existsSync(path)) {
+//     fs.readdirSync(path).forEach((file, index) => {
+//       const curPath = `${path}/${file}`
+//       if (fs.lstatSync(curPath).isDirectory()) {
+//         rimraf(curPath)
+//       } else {
+//         fs.unlinkSync(curPath)
+//       }
+//     })
+//     fs.rmdirSync(path)
+//   }
+// }
 
-const benchmarks = require(process.cwd() + '/benchmarks')
-const report = require('./report')
 
-if (argv.list) {
-  benchmarks.forEach(b => console.log(b.name))
-  process.exit()
+// Was this called directly from the CLI?
+// For mocha tests - maybe we can mitigate this but for now it works
+if (require.main == module) {
+  start(benchmarks, argv)
+  // rimraf('./ipfs-log-benchmarks')
 }
-
-const getElapsed = (time) => {
-  return +time[0] * 1e9 + +time[1]
-}
-
-const rimraf = (path) => {
-  if (fs.existsSync(path)) {
-    fs.readdirSync(path).forEach((file, index) => {
-      const curPath = `${path}/${file}`
-      if (fs.lstatSync(curPath).isDirectory()) {
-        rimraf(curPath)
-      } else {
-        fs.unlinkSync(curPath)
-      }
-    })
-    fs.rmdirSync(path)
-  }
-}
-
-const runOne = async (benchmark) => {
-  const stats = {
-    count: 0
-  }
-
-  if (global.gc) {
-    global.gc()
-  }
-
-  const memory = {
-    before: process.memoryUsage()
-  }
-
-  process.stdout.write(`\r${benchmark.name} / Preparing`)
-  const params = await benchmark.prepare()
-
-  process.stdout.clearLine()
-  const startTime = process.hrtime() // eventually convert to hrtime.bigint
-  while (benchmark.while({ stats, startTime, stressLimit, baselineLimit })) {
-    const elapsed = getElapsed(process.hrtime(startTime))
-    const totalSeconds = (elapsed / 1000000000).toFixed(4)
-    const opsPerSec = (stats.count / totalSeconds).toFixed(4)
-    process.stdout.write(`\r${benchmark.name} / Cycles: ${stats.count} (${opsPerSec.toString()} ops/sec)`)
-    await benchmark.cycle(params)
-    stats.count++
-  }
-
-  const elapsed = getElapsed(process.hrtime(startTime))
-  memory.after = process.memoryUsage()
-
-  process.stdout.write(`\r${benchmark.name} / Finishing`)
-  await benchmark.teardown(params)
-  process.stdout.clearLine()
-
-  return {
-    name: benchmark.name,
-    cpus: os.cpus(),
-    loadavg: os.loadavg(),
-    elapsed,
-    stats,
-    memory
-  }
-}
-
-const start = async () => {
-  const results = []
-  const baselineOnly = argv.baseline
-  const runnerStartTime = process.hrtime()
-
-  process.stdout.write(`Running ${baselineOnly ? 'baseline ' : ''}benchmarks matching: ${grep}`)
-  process.stdout.write('\n')
-
-  if (!global.gc) {
-    console.warn('start with --expose-gc')
-  }
-
-  try {
-    for (const benchmark of benchmarks) {
-      if (baselineOnly && !BASELINE_GREP.test(benchmark.name)) {
-        continue
-      }
-
-      if (!grep.test(benchmark.name)) {
-        continue
-      }
-
-      if (benchmark.count && benchmark.count > logLimit) {
-        continue
-      }
-
-      const result = await runOne(benchmark)
-      results.push(result)
-    }
-
-    const runnerElapsed = getElapsed(process.hrtime(runnerStartTime))
-    let output = `\rCompleted ${results.length} benchmark${results.length > 1 ? 's' : ''}`
-    output += ` in ${(runnerElapsed / 1000000000).toFixed(2)} seconds`
-    process.stdout.write(output)
-
-    if (argv.report) {
-      report(results)
-    }
-  } catch (e) {
-    console.log(e)
-  }
-
-  // TODO: compare/delta to cached version
-  rimraf('./ipfs-log-benchmarks')
-}
-
-start()
--- test/cli.test.js
+++ test/cli.test.js
@@ -3,8 +3,11 @@
 const yargs = require('yargs')
 const cli = require('../cli')
 const expect = require('chai').expect
+const { start } = require('../index')
 
-describe.skip('CLI Test', () => {
+const benchmarks = require(process.cwd() + '/benchmarks')
+
+describe('CLI Test', () => {
   it('should return help output', async () => {
     const parser = yargs.command(cli).help()
     const output = await new Promise((resolve) => {
@@ -23,8 +26,8 @@ describe.skip('CLI Test', () => {
     expect(output).to.contain('logLimit')
   })
 
-  describe.skip('baseline', () => {
-    it('should parse basline value', () => {
+  describe('baseline', () => {
+    it('should parse baseline value', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
         'cli --baseline', {}
@@ -33,7 +36,7 @@ describe.skip('CLI Test', () => {
       expect(yargsResult.b).to.be.true
     })
 
-    it('should parse basline value when using alias', () => {
+    it('should parse baseline value when using alias', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
         'cli -b', {}
@@ -41,9 +44,20 @@ describe.skip('CLI Test', () => {
       expect(yargsResult.baseline).to.be.true
       expect(yargsResult.b).to.be.true
     })
+
+    it('should run a baseline test', async () => {
+      const yargsCmd = yargs.command(cli)
+      const yargsResult = yargsCmd.parse(
+        'cli --baseline', {}
+      )
+
+      await start(benchmarks, yargsResult)
+    })
   })
 
-  describe.skip('report', () => {
+  describe('report', function () {
+    this.timeout(6000)
+
     it('should parse report value', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
@@ -61,9 +75,18 @@ describe.skip('CLI Test', () => {
       expect(yargsResult.report).to.be.true
       expect(yargsResult.r).to.be.true
     })
+
+    it('should generate report', async () => {
+      const yargsCmd = yargs.command(cli)
+      const yargsResult = yargsCmd.parse(
+        'cli -r', {}
+      )
+
+      await start(benchmarks, yargsResult)
+    })
   })
 
-  describe.skip('list', () => {
+  describe('list', function () {
     it('should parse list value', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
@@ -81,9 +104,18 @@ describe.skip('CLI Test', () => {
       expect(yargsResult.list).to.be.true
       expect(yargsResult.l).to.be.true
     })
+
+    it('should list benchmarks', async () => {
+      const yargsCmd = yargs.command(cli)
+      const yargsResult = yargsCmd.parse(
+        'cli -l', {}
+      )
+
+      await start(benchmarks, yargsResult)
+    })
   })
 
-  describe.skip('grep', () => {
+  describe('grep', () => {
     it('should parse grep value', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
@@ -119,15 +151,26 @@ describe.skip('CLI Test', () => {
       expect(output).to.contain('baselineLimit')
       expect(output).to.contain('logLimit')
     })
+
+    it('should properly apply grep value to benchmarks', async () => {
+      const yargsCmd = yargs.command(cli)
+      const yargsResult = yargsCmd.parse(
+        'cli --grep console-baseline', {}
+      )
+
+      await start(benchmarks, yargsResult)
+    })
   })
 
-  describe.skip('stressLimit', () => {
+  describe('stressLimit', function () {
+    this.timeout(11000)
+
     it('should parse stressLimit value', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
-        'cli --stressLimit 300', {}
+        'cli --stressLimit 10', {}
       )
-      expect(yargsResult.stressLimit).to.equal(300)
+      expect(yargsResult.stressLimit).to.equal(10)
     })
 
     it('should show help msg when arg not passed to stressLimit', async () => {
@@ -147,9 +190,20 @@ describe.skip('CLI Test', () => {
       expect(output).to.contain('baselineLimit')
       expect(output).to.contain('logLimit')
     })
+
+    it('should apply stressLimit arg', async () => {
+      const yargsCmd = yargs.command(cli)
+      const yargsResult = yargsCmd.parse(
+        'cli --stressLimit 10', {}
+      )
+
+      await start(benchmarks, yargsResult)
+    })
   })
 
-  describe.skip('baselineLimit', () => {
+  describe('baselineLimit', function () {
+    this.timeout(6000)
+
     it('should parse baselineLimit value', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
@@ -175,9 +229,20 @@ describe.skip('CLI Test', () => {
       expect(output).to.contain('baselineLimit')
       expect(output).to.contain('logLimit')
     })
+
+    it('should apply baseLineLimit arg', async () => {
+      const yargsCmd = yargs.command(cli)
+      const yargsResult = yargsCmd.parse(
+        'cli --baselineLimit 1000', {}
+      )
+
+      await start(benchmarks, yargsResult)
+    })
   })
 
-  describe.skip('logLimit', () => {
+  describe('logLimit', function () {
+    this.timeout(6000)
+
     it('should parse logLimit value', () => {
       const yargsCmd = yargs.command(cli)
       const yargsResult = yargsCmd.parse(
@@ -202,6 +267,15 @@ describe.skip('CLI Test', () => {
       expect(output).to.contain('stressLimit')
       expect(output).to.contain('baselineLimit')
       expect(output).to.contain('logLimit')
+    })
+
+    it('should apply logLimit arg', async () => {
+      const yargsCmd = yargs.command(cli)
+      const yargsResult = yargsCmd.parse(
+        'cli --logLimit 50', {}
+      )
+
+      await start(benchmarks, yargsResult)
     })
   })
 })
