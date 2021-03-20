1192ee3cf2589e9151da80028b809259378ddc37
tabcat <andersb@tuta.io>
2021-03-20 01:58:11 -0500

    automate browser benchmarks with puppeteer

--- src/exec-benchmark.browser.js
+++ src/exec-benchmark.browser.js
@@ -4,8 +4,13 @@ const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const middleware = require('webpack-dev-middleware')
 const express = require('express')
-// const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer')
 const webpackEntry = path.resolve(__dirname, 'webpack-entry.js')
+const defer = () => {
+  const d = {}
+  d.promise = new Promise(resolve => { d.resolve = resolve })
+  return d
+}
 
 module.exports = async function ({ file, host, basename }) {
   const compiler = webpack({
@@ -32,5 +37,14 @@ module.exports = async function ({ file, host, basename }) {
   instance.waitUntilValid(() => instance.close()) // turns off file watching
   app.use(instance)
   await new Promise(resolve => app.listen(3000, resolve))
-  await new Promise(() => {})
+
+  const deferred = defer()
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  page.on('error', e => process.stderr.write(e.toString()))
+  page.on('pageerror', e => process.stderr.write(e.toString()))
+  await page.exposeFunction('benchmarkerFinished', () => deferred.resolve())
+  await page.goto('http://localhost:3000')
+  await deferred.promise
+  await browser.close()
 }
--- src/webpack-entry.js
+++ src/webpack-entry.js
@@ -10,7 +10,7 @@ async function main () {
   const benchmarker = await BenchmarkerClient.create('${host}')
   await runBenchmark(benchmarker, benchmark, '${basename}')
 }
-main()
+main().then(() => window.benchmarkerFinished())
 `
   return { code }
 }
