2338cd49aaf92de60c2b0e0ed335528122b06653
tabcat <andersb@tuta.io>
2021-03-21 15:50:11 -0500

    move webpack to its own thread

--- src/exec-benchmark.browser.js
+++ src/exec-benchmark.browser.js
@@ -1,50 +1,29 @@
 'use strict'
 const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const middleware = require('webpack-dev-middleware')
-const express = require('express')
+const { fork } = require('child_process')
 const puppeteer = require('puppeteer')
-const webpackEntry = path.resolve(__dirname, 'webpack-entry.js')
-const defer = () => {
-  const d = {}
-  d.promise = new Promise(resolve => { d.resolve = resolve })
-  return d
+
+async function spawnWebpackServer ({ file: f, host: h, basename: b, port: p }) {
+  return new Promise(resolve => {
+    const subprocess = fork(
+      path.join(__dirname, 'webpack-server.js'),
+      [`-f${f}`, `-h${h}`, `-b${b}`, `-p${p}`],
+      { stdio: 'inherit' }
+    )
+    subprocess.on('message', m => m === 'listening' && resolve(subprocess))
+  })
 }
 
-module.exports = async function ({ file, host, basename }) {
-  const compiler = webpack({
-    mode: 'production',
-    entry: webpackEntry,
-    output: {
-      filename: 'bundle.js',
-      publicPath: '/'
-    },
-    module: {
-      rules: [
-        {
-          test: webpackEntry,
-          use: [{ loader: 'val-loader', options: { file, host, basename } }]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({ filename: 'index.html' })
-    ]
-  })
-  const app = express()
-  const instance = middleware(compiler)
-  instance.waitUntilValid(() => instance.close()) // turns off file watching
-  app.use(instance)
-  await new Promise(resolve => app.listen(3000, resolve))
-
-  const deferred = defer()
+module.exports = async function (opts) {
+  const webpackServer = await spawnWebpackServer({ port: 3000, ...opts })
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
   page.on('error', e => process.stderr.write(e.toString()))
   page.on('pageerror', e => process.stderr.write(e.toString()))
-  await page.exposeFunction('benchmarkerFinished', () => deferred.resolve())
-  await page.goto('http://localhost:3000')
-  await deferred.promise
+  await new Promise(resolve => {
+    page.exposeFunction('benchmarkerFinished', () => resolve())
+    page.goto('http://localhost:3000')
+  })
   await browser.close()
+  webpackServer.kill('SIGINT')
 }
---
+++ src/webpack-server.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+const path = require('path')
+const { program } = require('commander')
+program
+  .requiredOption('-f, --file <path>', 'the benchmark file to run')
+  .requiredOption('-h, --host <addr:port>', 'the address and port of the benchmarker server')
+  .requiredOption('-b, --basename <basename>', 'the file\'s name')
+  .option('-p, --port <port>', 'the port to host the bundled  at', 3000)
+program.parse()
+
+const { file, host, basename, port } = program.opts()
+
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const middleware = require('webpack-dev-middleware')
+const express = require('express')
+const webpackEntry = path.join(__dirname, 'webpack-entry.js')
+
+const compiler = webpack({
+  mode: 'production',
+  entry: webpackEntry,
+  output: { filename: 'bundle.js' },
+  module: {
+    rules: [
+      {
+        test: webpackEntry,
+        use: [{ loader: 'val-loader', options: { file, host, basename } }]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ filename: 'index.html' })]
+})
+
+const app = express()
+const instance = middleware(compiler)
+instance.waitUntilValid(() => instance.close()) // turns off file watching
+app.use(instance)
+app.listen(port, () => process.send && process.send('listening'))
